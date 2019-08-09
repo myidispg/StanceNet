ca459ab6c136f85b17528b1f6d8e2cb5eb11d508
Prashant Goyal <myidispg@gmail.com>
2019-08-09 17:00:35 +0530

    Added some code to visualize according to new dimensions

--- visualize_output.py
+++ visualize_output.py
@@ -12,31 +12,34 @@ import cv2
 import os
 
 
-from models.full_model import OpenPoseModel
+#from models.full_model import OpenPoseModel
+from models.paf_model import StanceNet
 from utilities.constants import MEAN, STD
 
 device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
 
-model = OpenPoseModel(15, 17).to(device).eval()
+#model = OpenPoseModel(15, 17).to(device).eval()
+model = StanceNet(17, 30, n_stages=5).eval()
 
 checkpoint = torch.load(os.path.join('trained_models', 'stancenet_1_epochs.pth'))
 model.load_state_dict(checkpoint['model_state'])
 
+model = model.to(torch.device('cpu'))
 
-img = cv2.imread(os.path.join('Coco_Dataset', 'train2017', '000000000241.jpg'))
+img = cv2.imread(os.path.join('Coco_Dataset', 'val2017', '000000000785.jpg'))
 img = ((img/255)-MEAN)/STD
 cv2.imshow('image', img)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-img = cv2.resize(img, (368, 368))
+img = cv2.resize(img, (400, 400))
 img = torch.from_numpy(img).view(1, img.shape[0], img.shape[1], img.shape[2]).permute(0, 3, 1, 2)
 
 img = img.to(device).float()
 
 outputs = model(img)
 
-for i in range(1, 6):
+for i in range(5):
     print(f'i: {i}, paf: {outputs[i]["paf"].sum()}')
     print(f'i: {i}, conf: {outputs[i]["conf"].sum()}')
 
@@ -50,24 +53,24 @@ paf = paf.transpose(2, 3, 1, 0)
 paf = paf.reshape(paf.shape[0], paf.shape[1], 2, -1)
 
 # Visualize Confidence map
-conf_map = np.zeros((92, 92))
+conf_map = np.zeros((50, 50))
 for i in range(conf.shape[2]):
     conf_map += conf[:, :, i]
 
-conf_map = cv2.resize(conf_map, (368, 368))
+conf_map = cv2.resize(conf_map, (400, 400))
 
 #conf_map = (conf_map > 0.7).astype(np.float32)
 
-cv2.imshow('conf map', conf_map*25500000)
+cv2.imshow('conf map', conf_map*255)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
 # Visualize Parts Affinity Fields
-paf_map = np.zeros((92, 92))
+paf_map = np.zeros((50, 50))
 for i in range(paf.shape[3]):
     paf_map += paf[:, :, 0, i] + paf[:, :, 1, i]
 
-paf_map = cv2.resize(paf_map, (368, 368))
+paf_map = cv2.resize(paf_map, (400, 400))
 
 paf_map = (paf_map > 0.01).astype(np.float32)
 
@@ -75,7 +78,12 @@ cv2.imshow('paf map', paf_map*255)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
+
 import matplotlib.pyplot as plt
+
+losses = checkpoint['losses']
+plt.plot(range(len(losses)), losses)
+plt.show()
 
 losses = [8334, 6148, 6241, 6658, 6073, 5948, 6669, 6380, 6299, 6538, 6187, 6219, 
           6779, 6176, 6524, 5560, 6555, 6405, 6586, 6329, 6507, 6667, 6426, 6840,
